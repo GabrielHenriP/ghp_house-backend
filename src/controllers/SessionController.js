3e56ef2e2ba2d2730f7f2f31efeb37f207660bb5
gabriel <soadhenri@gmail.com>
2021-01-04 10:53:00 -0300

    acessing mongodb and signing up email

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,27 @@
+
+// metodos: index, show, update, store, destroy
+/*
+index: listagem de sessão
+show: listar uma unica sessao
+store: criar uma sessao
+update: editar uma sessao
+destroy: deletar uma sessao
+*/
+
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res){
+    const { email } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if(!user){
+      user = await User.create({ email}) // ou ({ email: email })
+    }
+
+    return res.json(user);
+  }
+}
+
+export default new SessionController();
