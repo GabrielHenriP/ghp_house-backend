20772b6c7b3ccab98c0299789edb6062e2508075
gabriel <soadhenri@gmail.com>
2021-01-14 23:05:51 -0300

    creating a post route to store a houses register

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import upoloadConfig from './config/upload';
+
 import SessionController from './controllers/SessionController';
+import HouseController from './controllers/HouseController';
 
 const routes = Router();
+const upload = multer(upoloadConfig);
 
 routes.post('/sessions', SessionController.store)
+
+routes.post('/houses', upload.single('thumbnail'), HouseController.store)
 
 export default routes;
