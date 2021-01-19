a4e5786fcb6cb3196fdfe4fcfdaaaf2e23068f21
gabriel <soadhenri@gmail.com>
2021-01-19 17:27:49 -0300

    adding method index in Session Controller

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -11,6 +11,12 @@ destroy: deletar uma sessao
 import User from '../models/User';
 
 class SessionController {
+  async index(req, res){
+    const users = await User.find()
+
+    return res.json(users)
+  }
+
   async store(req, res){
     const { email } = req.body;
 
