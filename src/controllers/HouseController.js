987ab258ab502b0be8e8bdc6bc397fe962b6f66d
gabriel <soadhenri@gmail.com>
2021-01-19 17:46:19 -0300

    adding destroy method in HouseController

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -35,10 +35,10 @@ class HouseController{
 
     const user = await User.findById(user_id);
     const house = await House.findById(house_id)
-    console.log(user)
-    console.log(house)
 
-    
+    if(String(user._id) !== String(house.user)){
+      return res.status(401).json({ error: 'Não autorizado'})
+    }
 
     await House.updateOne({ _id: house_id }, {
       user: user_id,
@@ -49,6 +49,25 @@ class HouseController{
       status
     })
 
+    return res.send()
+  }
+
+  async destroy(req, res){
+    const { house_id } = req.body;
+    const { user_id} = req.headers;
+    
+    
+    const user = await User.findById(user_id);
+    const house = await House.findById(house_id)
+
+    if(String(user._id) !== String(house.user)){
+      return res.status(401).json({ error: 'Não autorizado'})
+    }
+
+    await House.findByIdAndDelete({ _id: house_id })
+
+
+    return res.json({ message: "Excluida com sucesso" })
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -9,9 +9,11 @@ const routes = Router();
 const upload = multer(upoloadConfig);
 
 routes.post('/sessions', SessionController.store);
+routes.get('/sessions', SessionController.index);
 
 routes.post('/houses', upload.single('thumbnail'), HouseController.store);
 routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.update);
 routes.get('/houses', HouseController.index);
+routes.delete('/houses', HouseController.destroy);
 
 export default routes;
